41685204e0ed43ccdb3dd18b4a07c502096c6652
Fernando Morales <81210670+NeroCc1@users.noreply.github.com>
2023-06-05 09:28:10 -0600

    Delete backend directory

--- backend/src/main/java/com/perreria/backend/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.perreria.backend.repository;
-
-import com.perreria.backend.model.Seguimiento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Seguimiento,Long>{
-}
